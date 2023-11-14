7eefbe8649f56c9e2c93df1961416bc9e91c3263
benbarker04 <>
2023-11-14 13:53:38 +0000

    change code in create_board function to make it easier to update the board in the future

--- run.py
+++ run.py
@@ -3,12 +3,12 @@ def create_board():
     Creates a board to be used in 2 seperate functions one to be used to make the player a board
     and another to be used to make the computer a board.
     """
-    print('  0 1 2 3 4 5 6 7 8 9')
+    print('   0 1 2 3 4 5 6 7 8 9')
     for x in range(10):
-        row = ''
+        row = [f'{x} ']
         for y in range(10):
-            empty_space =  '- '
-            row = row + empty_space
-        print(x, row)
+            empty_space =  '-'
+            row.append(empty_space)
+        print(' '.join(row))
 
 create_board()
