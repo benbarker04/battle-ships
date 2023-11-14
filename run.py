a7b9a68d2ef186711430be820e0266ad8d13b3bb
benbarker04 <>
2023-11-14 15:57:12 +0000

    add place_ships function to randomly place ships and make code easier to read by adding a class and splitting and creating the boards

--- run.py
+++ run.py
@@ -1,48 +1,57 @@
 import random
-from random import randint
 
+class Board:
+    def create_board(self):
+        """
+        Creates an empty board.
+        """
+        return [['.'] * self.size for _ in range(self.size)]
 
+    def __init__(self, name = None):
+        self.size = 10
+        self.empty_space = '.'
+        self.ship_symbol = '0'
+        self.name = name
+        self.board = self.create_board()
 
-def create_board():
-    """
-    Creates a board to be used in 2 seperate functions one to be used to make the player a board
-    and another to be used to make the computer a board.
-    """
-    print('   0 1 2 3 4 5 6 7 8 9')
-    for x in range(10):
-        row = [f'{x} ']
-        for y in range(10):
-            empty_space =  '.'
-            row.append(empty_space)
+    def print_board(self):
+        """
+        Prints the board with column labels.
+        """
+        print('   0 1 2 3 4 5 6 7 8 9')
+        for i, row in enumerate(self.board):
+            print(f'{i}  {" ".join(row)}')
+        if self.name:
+            print(f'\n{self.name}\'s Board')
 
-        boat = random.randint(1, 10)
-        row[boat] = '0'
-
-        print(' '.join(row))
-
-def create_player_board(name):
-    """
-    Creates board for the player.
-    """
-    print(f"      {name}'s board")
-    create_board()
-
-def create_bot_board():
-    """
-    Creates board for the computer.
-    """
-    print("       Bot's board")
-    create_board()
+    def place_ships(self, num_ships):
+        """
+        Randomly places the specified number of ships on the board.
+        """
+        for _ in range(num_ships):
+            row = random.randint(0, self.size - 1)
+            col = random.randint(0, self.size - 1)
+            while self.board[row][col] == self.ship_symbol:
+                row = random.randint(0, self.size - 1)
+                col = random.randint(0, self.size - 1)
+            self.board[row][col] = self.ship_symbol
 
 def new_game():
     """
-    starts a new game and prints everything needed on to the console
+    Starts a new game and prints everything needed on the console.
     """
     print('Welcome to Battle Ships!')
     print("First to destroy all of the other player's ships wins.\n")
-    name = input('Enter your name:')
-    print('\n')
-    create_player_board(name)
-    create_bot_board()
+    player_name = input('Enter your name: ')
+    print()
+
+    player_board = Board(name = player_name)
+    bot_board = Board(name = "Bot")
+
+    player_board.place_ships(num_ships = 10)
+    bot_board.place_ships(num_ships = 10)
+
+    player_board.print_board()
+    bot_board.print_board()
 
 new_game()
