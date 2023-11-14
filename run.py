9686a7c11aa7cb2472284bb7808b0c5598ec77f9
benbarker04 <>
2023-11-14 13:21:43 +0000

    add a function to make the battle ships board

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def create_board():
+    """
+    Creates a board to be used in 2 seperate functions one to be used to make the player a board
+    and another to be used to make the computer a board.
+    """
+    print('  0 1 2 3 4 5 6 7 8 9')
+    for x in range(10):
+        row = ''
+        for y in range(10):
+            empty_space =  '- '
+            row = row + empty_space
+        print(x, row)
+
+create_board()
